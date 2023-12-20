9a20cbd9e64170f7025bcd80c8100be1f0914694
ArtiFixal <89784139+ArtiFixal@users.noreply.github.com>
2023-12-20 01:18:48 +0100

    Add status code

--- src/main/java/meshservice/ServiceStatus.java
+++ src/main/java/meshservice/ServiceStatus.java
@@ -6,10 +6,21 @@ package meshservice;
  * @author ArtiFixal
  */
 public enum ServiceStatus {
-    STARTING,
-    RUNNING,
-    CLOSING,
-    CLOSED;
+    STARTING(0),
+    RUNNING(1),
+    CLOSING(2),
+    CLOSED(3);
+    
+    private final int statusCode;
+    
+    private ServiceStatus(int statusCode)
+    {
+        this.statusCode=statusCode;
+    }
+
+    public int getStatusCode(){
+        return statusCode;
+    }
     
     public static ServiceStatus interperFromNumber(int number)
     {
@@ -18,7 +29,18 @@ public enum ServiceStatus {
             case 1 -> ServiceStatus.RUNNING;
             case 2 -> ServiceStatus.CLOSING;
             case 3 -> ServiceStatus.CLOSED;
-            default -> throw new AssertionError();
+            default -> throw new IllegalArgumentException("Unknown status");
+        };
+    }
+
+    @Override
+    public String toString(){
+        return switch(statusCode){
+            case 0 -> "starting";
+            case 1 -> "running";
+            case 2 -> "closing";
+            case 3 -> "closed";
+            default -> throw new IllegalArgumentException("Unknown status");
         };
     }
 }
